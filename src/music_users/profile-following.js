94357f53245d03732fbe4a1ded9579f54b37cc5a
phamlo123 <pham.lo@northeastern.edu>
2023-01-21 01:57:45 -0500

    s

--- src/music_users/profile-following.js
+++ src/music_users/profile-following.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import {useDispatch, useSelector} from "react-redux";
-import {useLocation} from "react-router";
+import {useLocation} from "react-router-dom";
 import React, {useEffect} from "react";
 import {getPeopleUserFollowThunk} from "../home/who-thunk";
 import {Link} from "react-router-dom";
